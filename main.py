08b918ffbb00366f93579fe0426d7e729bd99268
Waqas Khalid Obeidy <waqasobeidy@gmail.com>
2024-11-15 20:35:09 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+class Message(BaseModel):
+    message: str
+
+@app.post("/post-chat")
+async def chat(message: Message):
+    return {"response": "You're awesome"}
+
+@app.get("/chat")
+async def chat():
+    return {"response": "You're awesome"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
